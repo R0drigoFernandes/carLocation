2ac5446923e7fbeef7bec4521c1e9d0ff047b3bc
Controle de Gastos <rod.frnnds@gmail.com>
2025-05-22 20:41:23 -0300

    carLocation

--- src/model/service/RentalService.java
+++ src/model/service/RentalService.java
@@ -8,7 +8,7 @@ import entities.Invoice;
 public class RentalService {
     private double pricePerHour;
     private double pricePerDay;
-    private TaxService taxService;
+    private final TaxService taxService;
     
     public RentalService(double pricePerHour, double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
@@ -35,7 +35,7 @@ public class RentalService {
        
        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
        double hours = minutes / 60.0;
-       double basicPayment = 0.0;
+       double basicPayment;
        if (hours <= 12.0) {
            basicPayment = pricePerHour * Math.ceil(hours);
        } else {
